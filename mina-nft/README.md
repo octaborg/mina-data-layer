38bfdccba39e455f43214899a61d301a17cf61e7
Vim Wickramasinghe <vimukthi@composable.finance>
2022-04-22 22:39:39 +0200

    Init

---
+++ mina-nft/README.md
@@ -0,0 +1,32 @@
+# Mina Snapp: Mui Snapp Template
+
+This is a Mina Snapp template built to work with [MUI](https://mui.com/). This template uses TypeScript.
+
+## How to build
+
+```sh
+npm run build
+```
+
+## How to Serve
+
+```sh
+npm run serve
+```
+
+## How to run tests
+
+```sh
+npm run test
+npm run testw # watch mode
+```
+
+## How to run coverage
+
+```sh
+npm run coverage
+```
+
+## License
+
+[Apache-2.0](LICENSE)
---
+++ mina-nft/src/App.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import Container from '@mui/material/Container';
+
+let Add;
+
+function App() {
+  let [snapp, setSnapp] = useState();
+  let [isLoading, setLoading] = useState(false);
+  let [isDeployed, setDeployed] = useState(false);
+  let [num, setNum] = useState("0");
+
+  async function deploy() {
+    if (isLoading) return;
+    setLoading(true);
+    Add = await import('../dist/add.js');
+    let snapp = await Add.deploy();
+    setLoading(false);
+    setDeployed(true);
+    setSnapp(snapp);
+    let state = await snapp.getSnappState();
+    setNum(state.num.toString());
+  }
+
+  async function handleClick() {
+    await snapp.update();
+    let state = await snapp.getSnappState();
+    setNum(state.num.toString());
+  }
+
+  return (
+    <Container fixed>
+      <Stack direction="column" spacing={2}>
+        <Chip label={num} variant="outlined" onClick={handleClick} disabled={!isDeployed}/>
+        <Button variant="contained" onClick={deploy} disabled={isDeployed}>Deploy</Button>
+      </Stack>
+    </Container>);
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
