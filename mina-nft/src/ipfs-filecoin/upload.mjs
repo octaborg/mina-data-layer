0405147c31254238bbf0f73a42fa68974b8359f5
gundeepsingh <gundeep.upenn@gmail.com>
2022-04-23 08:20:48 +0200

    Add the filefromPath

--- mina-nft/src/ipfs-filecoin/upload.mjs
+++ mina-nft/src/ipfs-filecoin/upload.mjs
@@ -5,12 +5,12 @@ import path from 'path'
 
 const NFT_STORAGE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGEyYTFlYkE4ODEwNzdFRDI2NzNEZDVGMmMxNkREZUZjRWIxRmM0QWEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1MDY2MTQzODA0NiwibmFtZSI6Im1pbmFuZnQifQ.Cfi0SIeNbGJ4sv5n9MesbOvRfVnJd8LZp9HqZVVO2lc'
 
-async function storeNFT(imagePath, name, description) {
+async function storeNFT( tokenID, imagePath, name, description) {
     // load the file from disk
     const image = await fileFromPath(imagePath)
 
     // create a new NFTStorage client using our API key
-    const nftstorage = new NFTStorage({ token: NFT_STORAGE_KEY })
+    const nftstorage = new NFTStorage({ token: tokenID })
 
     // call client.store, passing in the image & metadata
     return nftstorage.store({
@@ -18,4 +18,10 @@ async function storeNFT(imagePath, name, description) {
         name,
         description,
     })
+
+    async function fileFromPath(filePath) {
+        const content = await fs.promises.readFile(filePath)
+        const type = mime.getType(filePath)
+        return new File([content], path.basename(filePath), { type })
+    }
 }
